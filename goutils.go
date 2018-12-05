947ad0c95e2833ad28513f819db52e3ee54f341e
Mannix <jun2675731483@gmail.com>
2018-12-05 09:41:05 +0800

    no message

--- goutils.go
+++ goutils.go
@@ -2,7 +2,9 @@ package goutils
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"path"
 	"time"
 )
 
@@ -15,11 +17,24 @@ func RepeatTimer(d time.Duration, f func()) {
 }
 
 func CreateFileNotExist(filename string) (f *os.File, err error) {
-	if _, err = os.Stat(filename); !os.IsNotExist(err) {
-		err = errors.New("file is exist")
+	var (
+		base = path.Dir(filename)
+	)
+
+	_, err = os.Stat(base)
+	if err != nil {
+		err = os.MkdirAll(base, os.ModePerm)
+		if err != nil {
+			return
+		}
+	}
+
+	_, err = os.Stat(filename)
+	if err == nil {
+		err = errors.New(fmt.Sprint("%s is exist", filename))
 		return
 	}
-	
+
 	f, err = os.Create(filename)
 	if err != nil {
 		return
